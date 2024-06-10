676929e62df4772dc6e37a9c447ac2f482dda289
Adrian Edwards <adrian@adriancedwards.com>
2024-06-09 20:49:46 -0400

    refactor output logging into a function

--- nfsn-ddns.py
+++ nfsn-ddns.py
@@ -32,11 +32,16 @@ def doIPsMatch(ip1:IPAddress, ip2:IPAddress) -> bool:
     return ip1 == ip2
 
 
+def output(msg, type_msg=None):
+    timestamp = datetime.now().strftime("%y-%m-%d %H:%M:%S")  
+    type_str = f"{type_msg}: " if type_msg is not None else ""
+    print(f"{timestamp}: {type_str}{msg}")
+    
+
 def validateNFSNResponse(response):
     if response.get("error") is not None:
-        timestamp = datetime.now().strftime("%y-%m-%d %H:%M:%S")  
-        print(f"{timestamp}: ERROR: {response.get('error')}")
-        print(f"{timestamp}: ERROR: {response.get('debug')}")
+        output(response.get('error'), type_msg="ERROR")
+        output(response.get('debug'), type_msg="ERROR")
 
 
 def makeNFSNHTTPRequest(path, body, nfsn_username, nfsn_apikey):
