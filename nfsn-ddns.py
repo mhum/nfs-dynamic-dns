d9e03f087b3106d5cdfce9dbb68770d91d497835
Adrian Edwards <adrian@adriancedwards.com>
2024-06-09 20:05:44 -0400

    initial commit - start drafting it out

---
+++ nfsn-ddns.py
@@ -0,0 +1,28 @@
+import requests
+import argparse
+import os
+import ipaddress
+
+os.getenv('IP_PROVIDER', "http://ipinfo.io/ip")
+os.getenv('IPV6_PROVIDER', "http://v6.ipinfo.io/ip")
+os.getenv('USERNAME', "http://v6.ipinfo.io/ip")
+os.getenv('API_KEY', "http://v6.ipinfo.io/ip")
+os.getenv('DOMAIN', "http://v6.ipinfo.io/ip")
+# os.getenv('IPV6_PROVIDER', "http://v6.ipinfo.io/ip")
+# os.getenv('IPV6_PROVIDER', "http://v6.ipinfo.io/ip")
+
+
+
+
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='automate the updating of domain records to create Dynamic DNS for domains registered with NearlyFreeSpeech.net')
+	# parser.add_argument('integers', metavar='N', type=int, nargs='+',
+	# 					help='an integer for the accumulator')
+	# parser.add_argument('--sum', dest='accumulate', action='store_const',
+	# 					const=sum, default=max,
+	# 					help='sum the integers (default: find the max)')
+
+	args = parser.parse_args()
+	print(args.accumulate(args.integers))
